26d946904c8de36e3bafb4b87f9a6f5eea3294eb
Rajat Das <rajat@Rajats-MacBook-Air.local>
2024-09-06 20:51:07 +0530

    feat: template ui

--- youtube_downloader/settings.py
+++ youtube_downloader/settings.py
@@ -148,10 +148,16 @@ INSTALLED_APPS = [
     "storages",
 ]
 
+if DEBUG is True:
+    INSTALLED_APPS += ("corsheaders",)
+
+CORS_ORIGIN_ALLOW_ALL = DEBUG
+
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
