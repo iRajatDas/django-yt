08dc5c23e50210724f453f95b7f90faf8babd70f
Rajat Das <rajat@Rajats-MacBook-Air.local>
2024-09-08 19:25:40 +0530

    log

--- downloader/tasks.py
+++ downloader/tasks.py
@@ -132,7 +132,7 @@ def notify_progress_update(
     error_message=None,
 ):
     payload = {
-        "type": "progress_update",
+        "type": "progress.update",
         "stage": stage,
         "status": DownloadTask.objects.get(id=task_id).status,
         "task_id": str(task_id),
